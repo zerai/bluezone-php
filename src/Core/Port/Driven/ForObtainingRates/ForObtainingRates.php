82086657800a29e418f21dd8b330b7acc465f092
zerai <teclaizerai@gmail.com>
2024-08-04 11:35:53 +0200

    Init driven port 'ForObtainingRates'.

---
+++ src/Core/Port/Driven/ForObtainingRates/ForObtainingRates.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Bluezone\Core\Port\Driven\ForObtainingRates;
+
+/**
+ * DRIVEN PORT
+ */
+interface ForObtainingRates
+{
+    public function findAll(): array;
+
+    public function addRate(Rate $rate): void;
+
+    public function exists(string $rateName): bool;
+
+    /**
+     * UALFM
+     */
+    public function empty(): void;
+}
---
+++ src/Core/Port/Driven/ForObtainingRates/Rate.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Bluezone\Core\Port\Driven\ForObtainingRates;
+
+class Rate
+{
+    public string $name;
+
+    public int $amountPerHour;
+}
